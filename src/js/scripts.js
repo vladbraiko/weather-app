acd955f09f48add86cb30d31cc19211b9b043a08
Natalia Kiriunina <kiriunina.natalia@gmail.com>
2020-05-17 06:06:09 +0300

    part3-html-css

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,16 +1,16 @@
-const debounce = require('lodash.debounce');
-import cityList from '../json/city.list.json';
-const searchInput = document.querySelector('#js-search');
+// const debounce = require('lodash.debounce');
+// import cityList from '../json/city.list.json';
+// const searchInput = document.querySelector('#js-search');
 
-// Слушаем поиск и даем подсказку по названию города
-const inputСorrection = value => {
-  const arr = cityList.map(e => e.name).filter(e => e.indexOf(value) !== -1);
-  if (arr.length <= 5) {
-    console.log(arr);
-  }
-};
+// // Слушаем поиск и даем подсказку по названию города
+// const inputСorrection = value => {
+//   const arr = cityList.map(e => e.name).filter(e => e.indexOf(value) !== -1);
+//   if (arr.length <= 5) {
+//     console.log(arr);
+//   }
+// };
 
-searchInput.addEventListener(
-  'input',
-  debounce(() => inputСorrection(searchInput.value), 500),
-);
+// searchInput.addEventListener(
+//   'input',
+//   debounce(() => inputСorrection(searchInput.value), 500),
+// );
