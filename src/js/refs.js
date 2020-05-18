0c9be2f83c98a9050f97349b14fcd4a6da0ba152
Vladyslav Braiko <vladyslavbar@gmail.com>
2020-05-18 14:48:42 +0300

    fixed background image service

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,9 +1,7 @@
 const searchInput = document.querySelector('.search-location__form');
-const container = document.querySelector('.container');
 const backgroundWrapper = document.querySelector('.background-wrapper');
 
 export default {
   searchInput,
-  container,
   backgroundWrapper,
 };
