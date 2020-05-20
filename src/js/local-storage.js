9dc8cdc96287655920aee4964033d205c1ff9bd8
Viktor Karmazeniuk <victor.k9560@gmail.com>
2020-05-20 15:25:15 +0300

    added local storage

---
+++ src/js/local-storage.js
@@ -0,0 +1,22 @@
+const refs = {
+  addToLocalStorageBtn: document.querySelector('.search-location__form-btn'),
+  form: document.querySelector('.search-form'),
+};
+const favoriteSities = [];
+
+refs.addToLocalStorageBtn.addEventListener('click', addToLocalStorage);
+
+function addToLocalStorage() {
+  const city = refs.form.value;
+  refs.addToLocalStorageBtn.classList.add('search-location__form-btn-focus');
+
+  favoriteSities.push(city);
+  localStorage.setItem('city', JSON.stringify(favoriteSities));
+  refs.form.value = '';
+
+  setTimeout(clearClass, 800);
+}
+
+function clearClass() {
+  refs.addToLocalStorageBtn.classList.remove('search-location__form-btn-focus');
+}
