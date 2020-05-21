e950935963b61131b2479dd967861eb067cb41eb
Viktor Karmazeniuk <victor.k9560@gmail.com>
2020-05-21 20:32:42 +0300

    added local storage

--- src/js/refs.js
+++ src/js/refs.js
@@ -4,10 +4,16 @@ const addToLocalStorageBtn = document.querySelector(
   '.search-location__form-btn',
 );
 const form = document.querySelector('.search-form');
+const listOfButtons = document.querySelector('.search-location__slider-list');
+const btnPrev = document.querySelector('.search-location__slider-btnPrev');
+const btnNext = document.querySelector('.search-location__slider-btnNext');
 
 export default {
   searchInput,
   backgroundWrapper,
   addToLocalStorageBtn,
   form,
+  listOfButtons,
+  btnPrev,
+  btnNext,
 };
