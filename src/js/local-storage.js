eb89f18eb22ec243f5f2c4bf3412f7920d10c3b8
Viktor Karmazeniuk <victor.k9560@gmail.com>
2020-05-25 18:49:47 +0300

    reworked local storage

--- src/js/local-storage.js
+++ src/js/local-storage.js
@@ -1,19 +1,15 @@
 import refs from './refs.js';
 import storage from './cityService.js';
 import updateButtons from '../template/favorite-cities.hbs';
+import addCity from '../template/oneCity.hbs';
 import Siema from 'siema';
 
 createButtons(getLocalStorage());
-createSiema();
 
 refs.addToLocalStorageBtn.addEventListener('click', () => {
   addToLocalStorage();
-  createButtons(getLocalStorage());
-
-  createSiema();
 });
 
-//
 function addToLocalStorage() {
   const city = refs.form.value;
 
@@ -28,24 +24,51 @@ function addToLocalStorage() {
   refs.form.value = '';
 
   setTimeout(clearClass, 800);
+
+  const addNewButton = addCity(city);
+  const newElement = document.createElement('div');
+
+  newElement.innerHTML = addNewButton;
+
+  mySiema.append(newElement);
 }
 
-//
-//
+refs.listOfButtons.addEventListener('click', event => {
+  if (event.target.nodeName === 'path') {
+    const textContent = event.path[3].childNodes[1].textContent;
+    const indexForRemove = storage.favoriteCities.indexOf(textContent);
+
+    mySiema.remove(indexForRemove);
+
+    storage.favoriteCities.splice(indexForRemove, 1);
+
+    localStorage.setItem('city', JSON.stringify(storage.favoriteCities));
+  }
+
+  
+});
+
+const mySiema = new Siema({
+  selector: refs.listOfButtons,
+  perPage: {
+    279: 2,
+    767: 4,
+    1119: 4,
+  },
+  duration: 200,
+  draggable: false,
+  multipleDrag: false,
+  threshold: 20,
+  loop: false,
+});
+
+refs.btnPrev.addEventListener('click', () => mySiema.prev());
+refs.btnNext.addEventListener('click', () => mySiema.next());
+
 function clearClass() {
   refs.addToLocalStorageBtn.classList.remove('search-location__form-btn-focus');
 }
 
-//
-//
-function createButtons(cities) {
-  const markup = updateButtons(cities);
-
-  refs.listOfButtons.innerHTML = markup;
-}
-
-//
-//
 function getLocalStorage() {
   const arrayOfCities = localStorage.getItem('city');
 
@@ -59,34 +82,8 @@ function getLocalStorage() {
   return parsedCities;
 }
 
-function createSiema() {
-  const mySiema = new Siema({
-    selector: refs.listOfButtons,
-    perPage: {
-      279: 2,
-      767: 4,
-      1119: 4,
-    },
-    duration: 200,
-    draggable: false,
-    multipleDrag: false,
-    threshold: 20,
-    loop: false,
-  });
+function createButtons(cities) {
+  const markup = updateButtons(cities);
 
-  refs.btnPrev.addEventListener('click', () => mySiema.prev());
-  refs.btnNext.addEventListener('click', () => mySiema.next());
-
-  refs.listOfButtons.addEventListener('click', event => {
-    if (event.target.nodeName === 'path') {
-      const textContent = event.path[3].childNodes[1].textContent;
-      const indexForRemove = storage.favoriteCities.indexOf(textContent);
-
-      mySiema.remove(indexForRemove);
-
-      storage.favoriteCities.splice(indexForRemove, 1);
-
-      localStorage.setItem('city', JSON.stringify(storage.favoriteCities));
-    }
-  });
+  refs.listOfButtons.insertAdjacentHTML('beforeend', markup);
 }
